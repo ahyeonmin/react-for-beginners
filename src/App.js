a6dd431f8b658eb651b171bf564c3b14f102dbbb
AHYEON <yotu00@naver.com>
2023-03-08 15:12:21 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return(
     <Router>
       <Routes basename={process.env.PUBLIC_URL}>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </Router>
   );
