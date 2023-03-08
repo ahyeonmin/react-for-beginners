3665de4eb6af89ca2bbff2309083792be133ed79
AHYEON <yotu00@naver.com>
2023-03-08 15:16:14 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Detail from "./routes/Detail";
 function App() {
   return(
     <Router>
-      <Routes basename={process.env.PUBLIC_URL}>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<Detail />} />
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
       </Routes>
     </Router>
   );
