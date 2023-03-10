eba260d36ec616154b764335ec56f9d1226025d2
AHYEON <yotu00@naver.com>
2023-03-10 16:18:30 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return(
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
